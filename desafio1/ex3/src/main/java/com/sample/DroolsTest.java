3a47c1c403e6aa18f34f62251e841b01edac5f84
Pedro Matos <pedrolopesmatos@hotmail.com>
2021-04-25 21:36:40 +0100

    Desafio 1 (final)

--- desafio1/ex3/src/main/java/com/sample/DroolsTest.java
+++ desafio1/ex3/src/main/java/com/sample/DroolsTest.java
@@ -20,33 +20,41 @@ public class DroolsTest {
         	Pessoa joaquim = new Pessoa("Joaquim");
         	
         	boolean estadoInicial = false;
-        	Sala sala = new Sala(18, estadoInicial);
-        	kSession.insert(jose);
+        	Sala sala = new Sala(19, estadoInicial);
+        	
+        	System.out.println(String.format(" -> Sistema está %s", estadoInicial ? "ligado" : "desligado"));
+    		System.out.println(String.format(" -> Temperatura ambiente: %d\n", sala.getTemperatura()));
+        	
         	kSession.insert(manuel);
+        	kSession.insert(jose);
         	kSession.insert(joaquim);
         	kSession.insert(sala);
         	kSession.fireAllRules();
         	
-        	
-        	/*
-        	Sala sala;
-        	for (int temp = 8; temp <= 10; temp += 2) {
-        		for(boolean estadoInicial : new boolean[] {true, false}) {
-            		System.out.println(String.format("|| Temperatura da sala: %d ºC ||", temp));
-            		System.out.println(String.format("|| Sistema está %s ||\n", estadoInicial ? "ligado" : "desligado"));
-            		sala = new Sala(temp, estadoInicial);
-            		
-            		kSession.insert(jose);
-                	kSession.insert(manuel);
-                	kSession.insert(joaquim);
-                	kSession.insert(sala);
-                	kSession.fireAllRules();
-        		}            	
-        	}
-        	*/
         } catch (Throwable t) {
             t.printStackTrace();
         }
     }
-
 }
+
+
+
+
+
+
+/*
+Sala sala;
+for (int temp = 8; temp <= 10; temp += 2) {
+	for(boolean estadoInicial : new boolean[] {true, false}) {
+		System.out.println(String.format("|| Temperatura da sala: %d ºC ||", temp));
+		System.out.println(String.format("|| Sistema está %s ||\n", estadoInicial ? "ligado" : "desligado"));
+		sala = new Sala(temp, estadoInicial);
+		
+		kSession.insert(jose);
+    	kSession.insert(manuel);
+    	kSession.insert(joaquim);
+    	kSession.insert(sala);
+    	kSession.fireAllRules();
+	}            	
+}
+*/
